9c61f9f119f9c22ffb92ec2a276c52248428d100
kaolalizheng <3370652176@qq.com>
2023-06-04 15:22:29 +0800

    新增页面

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="yangshi">
+    <p>{{ a }}</p>
+    <button @click="add">增加</button>
+    <button @click="minus">减少</button>
+    <button @click="division">除去</button>
+    <p>{{ doubleA }}</p>
+    <p>{{ squareA }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        a:100
+      }
+    },
+    methods:{
+      add(){
+        this.a++;
+      },
+      minus(){
+        this.a--
+      },
+      division(){
+        this.a=this.a/2;
+      }
+    },
+    computed:{
+      doubleA(){
+        return this.a*2;
+      },
+      squareA(){
+        return this.a*this.a;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.yangshi{
+  background-color: aqua;
+  font-size: 25px;
+}
+</style>
