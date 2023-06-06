f21362416ee705e057965f2df6cd0d394ca721ad
kaolalizheng <122143656+kaolalizheng@users.noreply.github.com>
2023-06-06 09:36:31 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,6 @@
 <template>
   <div class="yangshi">
     <p>{{ a }}</p>
-    <button @click="add">增加</button>
-    <button @click="minus">减少</button>
-    <button @click="division">除去</button>
-    <p>{{ doubleA }}</p>
-    <p>{{ squareA }}</p>
-    <p>********************</p>
-    <p>{{ b }}</p>
-    <button @click="change">改变</button>
   </div>
 </template>
 
@@ -18,43 +10,9 @@
       return {
         a:100
       }
-    },
-    created(){
-      console.log('我是前端的新增');
-    },
-    methods:{
-      add(){
-        this.a++;
-      },
-      minus(){
-        this.a--
-      },
-      division(){
-        this.a=this.a/2;
-      },
-      change(){
-        this.b='刘德华'
-      }
-    },
-    computed:{
-      doubleA(){
-        return this.a*2;
-      },
-      squareA(){
-        return this.a*this.a;
-      }
     }
   }
 </script>
 
 <style scoped>
-.yangshi{
-  background-color: aqua;
-  font-size: 25px;
-}
-p{
-  background: red;
-  font-weight: bold;
-  color: aqua;
-}
 </style>
